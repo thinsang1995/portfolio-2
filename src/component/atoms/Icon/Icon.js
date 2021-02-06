b1809aae1f861bac15e66a00d681644736315033
Thin Sang <thinsang1995@gmail.com>
2021-02-06 22:02:05 +0900

    inital and create Icon

---
+++ src/component/atoms/Icon/Icon.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Icon.css'
+import { FaInstagramSquare, FaTwitter, FaYoutube, FaLinkedin, FaCloudDownloadAlt } from "react-icons/fa";
+import { DiHtml5, DiCss3, DiSass, DiJavascript1, DiReact } from 'react-icons/di';
+import { SiRedux } from 'react-icons/si';
+import { BsFillPeopleFill } from 'react-icons/bs';
+import { HiSpeakerphone }  from 'react-icons/hi';
+import { AiFillLike, AiFillFacebook } from 'react-icons/ai';
+
+const icons = {
+  facebook: AiFillFacebook,
+  instagram: FaInstagramSquare,
+  twitter: FaTwitter,
+  linkedin: FaLinkedin,
+  youtube: FaYoutube,
+  html: DiHtml5,
+  css: DiCss3,
+  sass: DiSass,
+  javascript: DiJavascript1,
+  react: DiReact,
+  redux: SiRedux,
+  cloudDownload: FaCloudDownloadAlt,
+  people: BsFillPeopleFill,
+  speaker: HiSpeakerphone,
+  like: AiFillLike,
+}
+
+const Icon = (props) => {
+  const IconComponent = icons[props.iconKey]
+  const className = Object.keys(props).reduce((arr, key) => {
+    return props[key] ? [...arr, key] : arr
+  }, ['icon-wrapper']).join(' ')
+
+  return (
+    <div className={props.iconKey} className={className} >
+      <IconComponent />
+    </div>
+  )
+}
+
+export default Icon
---
+++ src/component/molecules/HomeIcons/HomeIcons.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Icon from '../../atoms/Icon'
+
+const HomeIcons = () => {
+  const icons = ['facebook', 'instagram', 'twitter', 'linkedin', 'youtube']
+
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      {icons.map(icon => (
+        <Icon 
+          iconKey={icon} 
+          key={icon} 
+          isSmall 
+          isFill
+        />
+      ))}
+    </div>
+  )
+}
+
+export default HomeIcons
