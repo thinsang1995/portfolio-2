559460805f9524cb7b8050d164249acc599d73b2
Thin Sang <thinsang1995@gmail.com>
2021-02-15 10:49:05 +0900

    change image

--- src/component/atoms/Image/Image.jsx
+++ src/component/atoms/Image/Image.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import aboutImg from '../../../assets/avata-picture.png'
-import portfolioImg1 from '../../../assets/portfolio-1.png'
-import portfolioImg2 from '../../../assets/portfolio-2.png'
-import portfolioImg3 from '../../../assets/portfolio-3.png'
-import portfolioImg4 from '../../../assets/portfolio-4.png'
-import portfolioImg5 from '../../../assets/portfolio-5.png'
-import portfolioImg6 from '../../../assets/portfolio-6.png'
+import portfolioImg1 from '../../../assets/portfolio-1.jpeg'
+import portfolioImg2 from '../../../assets/portfolio-2.jpg'
+import portfolioImg3 from '../../../assets/portfolio-3.jpg'
+import portfolioImg4 from '../../../assets/portfolio-4.jpg'
+import portfolioImg5 from '../../../assets/portfolio-5.jpg'
+import portfolioImg6 from '../../../assets/portfolio-6.jpg'
 import blogImg1 from '../../../assets/blog-1.jpg'
 import blogImg2 from '../../../assets/blog-2.jpeg'
 import blogImg3 from '../../../assets/blog-3.jpg'
 import blogImg4 from '../../../assets/blog-4.jpg'
-import customerImg from '../../../assets/customer-avata.png'
+import customerImg1 from '../../../assets/customer-avata-1.png'
+import customerImg2 from '../../../assets/customer-avata-2.jpg'
+import customerImg3 from '../../../assets/customer-avata-3.png'
 
 const images = {
   aboutAvata: aboutImg,
@@ -24,7 +26,9 @@ const images = {
   blogImageNum2: blogImg2,
   blogImageNum3: blogImg3,
   blogImageNum4: blogImg4,
-  customerImage: customerImg,
+  customerImage1: customerImg1,
+  customerImage2: customerImg2,
+  customerImage3: customerImg3,
 }
 
 const Image = ({ imageName }) => {
