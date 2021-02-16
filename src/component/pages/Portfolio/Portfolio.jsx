db1de2928bf9d3406efe8f8a693016bed6388ac4
Thin Sang <thinsang1995@gmail.com>
2021-02-17 07:15:59 +0900

    correct WorkFilterNav to WorkNav

--- src/component/pages/Portfolio/Portfolio.jsx
+++ src/component/pages/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react'
 import ContainerWrapper from '../../atoms/ContainerWrapper'
-import WorkFilterNav from '../../molecules/WorkFilterNav'
+import WorkNav from '../../molecules/WorkNav'
 import WorkList from '../../organisms/WorkList'
 
 const Portfolio = (props, ref) => {
@@ -8,7 +8,7 @@ const Portfolio = (props, ref) => {
     <ContainerWrapper ref={ref}>
       <div>
         <h2>Portfolio.</h2>
-        <WorkFilterNav />
+        <WorkNav />
         <WorkList />
       </div>
     </ContainerWrapper>
