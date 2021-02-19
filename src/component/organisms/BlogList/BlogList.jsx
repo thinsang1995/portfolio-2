cad56f3f5de353573f1e9e3b6bd3a3fa57fd316d
Thin Sang <thinsang1995@gmail.com>
2021-02-20 00:18:01 +0900

    responsive and auto slide blog item

--- src/component/organisms/BlogList/BlogList.jsx
+++ src/component/organisms/BlogList/BlogList.jsx
@@ -17,14 +17,24 @@ const BlogList = () => {
 
   const settings = {
     dots: true,
-    infinite: false,
-    speed: 500,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -32,7 +42,7 @@ const BlogList = () => {
         }
       },
       {
-        breakpoint: 500,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
