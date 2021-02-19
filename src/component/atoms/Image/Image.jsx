3a9a0728a20e79e1863c0acc26262d6801f51ca4
Thin Sang <thinsang1995@gmail.com>
2021-02-19 23:18:39 +0900

    move image link

--- src/component/atoms/Image/Image.jsx
+++ src/component/atoms/Image/Image.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import blogImg1 from '../../../assets/blog-1.jpg'
-import blogImg2 from '../../../assets/blog-2.jpeg'
-import blogImg3 from '../../../assets/blog-3.jpg'
-import blogImg4 from '../../../assets/blog-4.jpg'
 
 const Image = ({ imageSrc }) => {
   return <img src={imageSrc} alt={imageSrc} />
