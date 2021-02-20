7940b384f19d6a08aeadec1b6529e42adfc8b377
Thin Sang <thinsang1995@gmail.com>
2021-02-20 18:53:59 +0900

    add Contact and Footer page

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ import Portfolio from './component/pages/Portfolio'
 import Clients from './component/pages/Clients'
 import Blog from './component/pages/Blog'
 import Contact from './component/pages/Contact'
+import Footer from './component/pages/Footer'
 
 
 const links = ['home', 'about', 'services', 'portfolio', 'clients', 'blog', 'contact']
 
 const aboutDescription = "I am Thin Sang Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever It has survived not only five centuries, but also the leap into electronic  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing"
 
+const aboutButtonName = 'Contact Me'
+const contactButtonName = 'Submit'
+
 const greyColor = '#f7f7f7'
+
+const footerContent = 'Copy Right 2018 © By Daniels All Rights Reserved'
 
 function App() {
   const aboutRef = useRef(null)
@@ -54,7 +60,7 @@ function App() {
   }
 
   return (
-    <div style={{ width: '100vw', height: '710vh' }}>
+    <div style={{ width: '100vw', height: 'auto' }}>
       <Navbar 
         links={links} 
         handleScrollToEl={handleScrollToEl}
@@ -62,12 +68,13 @@ function App() {
         isShow={isShow}
       />
       <Home backgroundImage={backgroundImage} ref={homeRef} />
-      <About imageSrc={aboutImg} content={aboutDescription} ref={aboutRef} />
+      <About imageSrc={aboutImg} content={aboutDescription} buttonName={aboutButtonName} ref={aboutRef} />
       <Services backgroundColor={greyColor} ref={servicesRef} />
       <Portfolio ref={portfolioRef} />
       <Clients ref={clientsRef} />
       <Blog ref={blogRef} />
-      <Contact />
+      <Contact buttonName={contactButtonName} ref={contactRef} />
+      <Footer content={footerContent} />
     </div>
   );
 }
