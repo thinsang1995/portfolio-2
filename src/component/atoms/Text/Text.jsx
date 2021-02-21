e282e4878ab687bc3bdd4e0b55dd2ffa97e5c064
Thin Sang <thinsang1995@gmail.com>
2021-02-21 15:57:20 +0900

    responsive fontsize

--- src/component/atoms/Text/Text.jsx
+++ src/component/atoms/Text/Text.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
+import './Text.css'
 
 const Text = ({ content }) => {
-  return <p 
-    style={{
-      margin: '0',
-      lineHeight: '1.6',
-      fontSize: '1rem',
-      color: '#748182',
-      wordSpacing: '1.5px'
-    }}
-  >
-    {content}
-  </p>
+  return <p className='text'>{content}</p>
 }
 
 export default Text
