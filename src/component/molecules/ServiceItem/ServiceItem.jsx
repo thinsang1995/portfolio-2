8230b2a5a554eb50838d8af9292f377b37a6cb7c
Thin Sang <thinsang1995@gmail.com>
2021-02-21 15:59:23 +0900

    add animation flip card and resposive service list

--- src/component/molecules/ServiceItem/ServiceItem.jsx
+++ src/component/molecules/ServiceItem/ServiceItem.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
+import './ServiceItem.css'
 import Icon from '../../atoms/Icon'
 import Text from '../../atoms/Text'
 
 const ServicesItem = ({ icon, title, content }) => {
   return (
-    <div 
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        padding: '3rem 2rem',
-        boxShadow: '0px 10px 30px -8px rgb(0 0 0 / 10%)',
-        borderRadius: '10px',
-        backgroundColor: 'white',
-      }}
-    >
-      <Icon iconKey={icon} />
-      <h4 
-        style={{
-          margin: '1.1rem 0', 
-          fontSize: '20px', 
-          fontWeight: '800'
-        }}
-      >
-        {title}
-      </h4>
-      <Text content={content} />
+    <div className='service-item'>
+      <div className='item-flip'>
+        <div className='item-front'>
+          <Icon iconKey={icon} />
+          <h4>{title}</h4>
+        </div>
+        <div className='item-back'>
+          <Text content={content} />
+        </div>
+      </div>
     </div>
   )
 }
