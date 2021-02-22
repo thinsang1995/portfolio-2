cdc01dd6205e9c7cefbf5accbef08c83f8a5daaa
Thin Sang <thinsang1995@gmail.com>
2021-02-22 23:07:20 +0900

    responsive for Contact page

--- src/component/pages/Contact/Contact.jsx
+++ src/component/pages/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React, { forwardRef } from 'react'
+import './Contact.css'
 import ContactForm from '../../organisms/ContactForm'
 import ContactInfo from '../../organisms/ContactInfo'
 
 const Contact = ({ buttonName }, ref) => {
   return (
-    <div style={{padding: '9vh 0'}} ref={ref}>
+    <div className="contact" ref={ref}>
       <h2>Contact.</h2>
       <ContactInfo />
       <ContactForm buttonName={buttonName} />
