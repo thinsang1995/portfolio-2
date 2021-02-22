4383586db91c6b3688fe036a38687d4a8b0b1cfc
Thin Sang <thinsang1995@gmail.com>
2021-02-22 23:05:40 +0900

    responsive for mobile

--- src/component/organisms/AboutDescription/AboutDescription.jsx
+++ src/component/organisms/AboutDescription/AboutDescription.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
+import './AboutDescription.css'
 import Button from '../../atoms/Button'
 import Skills from '../../molecules/Skills'
 import Text from '../../atoms/Text'
 
 const AboutDescription = ({ content, buttonName }) => {
   return (
-    <div>
-      <h3 style={{fontSize: '30px'}}>About Me.</h3>
-      <h5 
-        style={{
-          margin: '0',
-          marginBottom: '1rem',
-          color: '#999',
-          letterSpacing: '2px',
-          lineHeight: '1.6',
-          fontWeight: '400',
-          fontSize: '18px'
-        }}
-      >
+    <div className='about-desc'>
+      <h3>About Me.</h3>
+      <h5>
         FRONT END DEVELOPER
       </h5>
       <Text content={content} />
