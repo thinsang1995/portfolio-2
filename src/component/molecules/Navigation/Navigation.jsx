9965d56cb9ae593e5c42ff6ecb46c161da0467e7
Thin Sang <thinsang1995@gmail.com>
2021-02-23 22:12:53 +0900

    adding active navigation link

--- src/component/molecules/Navigation/Navigation.jsx
+++ src/component/molecules/Navigation/Navigation.jsx
@@ -5,7 +5,14 @@ const Navigation = ({ links, handleScrollToEl, isShow }) => {
   return (
     <div className={`navigation ${isShow ? 'isShow' : ''}`}>
       {links.map(link => (
-          <span key={link} id={link} onClick={handleScrollToEl}>{link.charAt(0).toUpperCase() + link.slice(1)}</span>
+          <span 
+            className={`navigation-${link}`}
+            key={link} 
+            id={link} 
+            onClick={handleScrollToEl}
+          >
+            {link.charAt(0).toUpperCase() + link.slice(1)}
+          </span>
         )
       )}
     </div>
--- src/component/organisms/Navbar/Navbar.jsx
+++ src/component/organisms/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Navbar.css'
 import Navigation from '../../molecules/Navigation'
 
 const Navbar = ({ links, handleScrollToEl, handleShowNavItem, isShow }) => {
+
+  useEffect(() => {
+    const sections = document.querySelectorAll('section')
+
+    const handleActiveNavigation = () => {
+      sections.forEach(section => {
+        let sectionHeight = section.offsetHeight
+        let sectionTop = section.offsetTop - 100
+
+        if((window.pageYOffset > sectionTop) && (window.pageYOffset <= sectionTop + sectionHeight)) {
+          document.querySelector('.navigation-' + section.childNodes[0].id).classList.add('active')
+        } else {
+          document.querySelector('.navigation-' + section.childNodes[0].id).classList.remove('active')
+        }
+      })
+    }
+    window.addEventListener('scroll', handleActiveNavigation)
+  })
+
   return (
     <div className='nav-bar'>
       <h2>Sang</h2>
-      <Navigation links={links} handleScrollToEl={handleScrollToEl} isShow={isShow} />
+      <Navigation 
+        links={links} 
+        handleScrollToEl={handleScrollToEl} 
+        isShow={isShow}
+      />
       <div className='hamburger' onClick={handleShowNavItem}>
         <div className="line"></div>
         <div className="line"></div>
