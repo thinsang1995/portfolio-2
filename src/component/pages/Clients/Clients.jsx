6fd3c1ecf49a357633a600891d0f5248e13e1ce6
Thin Sang <thinsang1995@gmail.com>
2021-02-23 17:20:12 +0900

    remove inline style

--- src/component/pages/Clients/Clients.jsx
+++ src/component/pages/Clients/Clients.jsx
@@ -4,7 +4,7 @@ import Feedbacks from '../../organisms/Feedbacks'
 
 const Clients = (props, ref) => {
   return (
-    <div style={{height: 'auto'}}>
+    <div>
       <div
         ref={ref}
         style={{
