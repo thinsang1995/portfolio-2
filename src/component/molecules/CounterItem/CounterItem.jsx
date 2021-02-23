da73c812886c110bbd1e95cd94d668b869e99f12
Thin Sang <thinsang1995@gmail.com>
2021-02-23 09:04:24 +0900

    responsive and external style css

--- src/component/molecules/CounterItem/CounterItem.jsx
+++ src/component/molecules/CounterItem/CounterItem.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
+import './CounterItem.css'
 import Icon from '../../atoms/Icon'
 import Text from '../../atoms/Text'
 
 const CounterItem = ({ iconKey, number, title }) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontWeight: '600',
-        margin: '1rem 0',
-        width: '25vw'
-      }}
-    >
+    <div className='counter-item'>
       <Icon iconKey={iconKey} />
-      <h3 
-        style={{
-          margin: '0.8rem 0', 
-          fontSize: '1.8rem', 
-          fontWeight: '900'
-        }}
-      >{number}</h3>
+      <h3>{number}</h3>
       <Text content={title} />
     </div>
   )
