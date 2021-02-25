9540934defa7d569d318ce1f21083af5b8c8ca36
Thin Sang <thinsang1995@gmail.com>
2021-02-25 19:46:10 +0900

    handle props index and handlModelImage

--- src/component/molecules/WorkImage/WorkImage.jsx
+++ src/component/molecules/WorkImage/WorkImage.jsx
@@ -5,7 +5,7 @@ import Image from '../../atoms/Image'
 
 const overlayIcons = ['link', 'zoom']
 
-const WorkImage = ({ imageSrc, imageTitle }) => {
+const WorkImage = ({ imageSrc, imageTitle, handleModelImage, index }) => {
   return (
     <div className='work-image'>
       <Image imageSrc={imageSrc} />
@@ -14,7 +14,16 @@ const WorkImage = ({ imageSrc, imageTitle }) => {
           <h6>{imageTitle}</h6>
           <div className='work-icons'>
             {overlayIcons.map(icon => (
-              <Icon key={icon} iconKey={icon} isSmall isFill />
+              <Icon
+                key={icon} 
+                iconKey={icon} 
+                isSmall 
+                isFill
+                index={index}
+                handleModelImage={
+                  icon === overlayIcons[1] ? handleModelImage : () => {}
+                }
+              />
             ))}
           </div>
         </div>
