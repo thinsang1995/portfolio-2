b5a0fd868e8dde4da5db87c6743836e6b0e7f8d8
Thin Sang <thinsang1995@gmail.com>
2021-02-25 19:47:46 +0900

    lifting state up works data

--- src/component/organisms/WorkList/WorkList.jsx
+++ src/component/organisms/WorkList/WorkList.jsx
@@ -1,30 +1,17 @@
 import React from 'react'
 import './WorkList.css'
 import WorkImage from '../../molecules/WorkImage'
-import portfolioImg1 from '../../../assets/portfolio-1.jpeg'
-import portfolioImg2 from '../../../assets/portfolio-2.jpg'
-import portfolioImg3 from '../../../assets/portfolio-3.jpg'
-import portfolioImg4 from '../../../assets/portfolio-4.jpg'
-import portfolioImg5 from '../../../assets/portfolio-5.jpg'
-import portfolioImg6 from '../../../assets/portfolio-6.jpg'
 
-const works = [
-  {imageSrc: portfolioImg1, imageTitle: 'WEB DESIGN'},
-  {imageSrc: portfolioImg2, imageTitle: 'UX/UI'},
-  {imageSrc: portfolioImg3, imageTitle: 'PHOTOSHOP'},
-  {imageSrc: portfolioImg4, imageTitle: 'WEB DESIGN'},
-  {imageSrc: portfolioImg5, imageTitle: 'UX/UI'},
-  {imageSrc: portfolioImg6, imageTitle: 'WEB DESIGN'},
-]
-
-const WorkList = () => {
+const WorkList = ({ works, handleModelImage }) => {
   return (
     <div className='work-list'>
-      {works.map(item => (
+      {works.map((item, index) => (
         <WorkImage 
           key={item.imageSrc}
           imageSrc={item.imageSrc} 
           imageTitle={item.imageTitle} 
+          handleModelImage={handleModelImage}
+          index={index}
         />
       ))}
     </div>
